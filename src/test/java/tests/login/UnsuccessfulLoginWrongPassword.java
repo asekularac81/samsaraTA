801013675439971be3957a971472464f0fd6924b
Ana Sekularac <end02014@adobe.com>
2022-05-31 19:04:55 +0200

    Adding new test for unsuccessful Login

---
+++ src/test/java/tests/login/UnsuccessfulLoginWrongPassword.java
@@ -0,0 +1,60 @@
+package tests.login;
+
+import Data.CommonStrings;
+import Pages.LoginPage;
+import Utils.PropertiesUtils;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+@Test(groups = {"regression", "sanity", "login"})
+public class UnsuccessfulLoginWrongPassword extends BaseTest {
+
+  private String sTestName = this.getClass().getName();
+
+
+  private WebDriver driver;
+
+  private String sUserName;
+  private String sInvalidPassword;
+
+  @BeforeMethod
+  public void setUpTest () {
+    log.info("[SETUP TEST] " + sTestName );
+    driver = setupDriver();
+    sUserName = PropertiesUtils.getAdminUsername();
+    sInvalidPassword = PropertiesUtils.getAdminPassword() +"!";
+  }
+
+  @Test
+  public void testUnsuccessfulLogin() {
+
+    log.info("[START TEST] " + sTestName);
+    String sExpectedLoginErrorMessage = CommonStrings.LOGIN_ERROR_MESSAGE;
+
+    log.info("Open Login page.");
+    LoginPage loginPage = new LoginPage(driver).open();
+
+    log.info("Type username and invalid password.");
+    loginPage.typeUsername(sUserName);
+    loginPage.typePassword(sInvalidPassword);
+
+    // Iako ostajemo na istoj stranici, uvek treba novu instancy vratiti u promenjivu loginPage
+    // Time se stitimo od Stale element refrence koja se desava kad jer se osvezila DOM struktura stranice nakon neke nase akcije a mi se oslanjamo na staru referencu
+    log.info("Click Login Button and verify error message.");
+    loginPage = loginPage.clickLoginButtonNoProgress();
+    String sSuccessMessage = loginPage.getErrorMessage();
+    Assert.assertEquals(sSuccessMessage, sExpectedLoginErrorMessage, "Wrong Login Error Message!");
+
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDownTest (ITestResult testResult) {
+    log.info("[END TEST] " + sTestName);
+    tearDown(driver, testResult);
+  }
+}
